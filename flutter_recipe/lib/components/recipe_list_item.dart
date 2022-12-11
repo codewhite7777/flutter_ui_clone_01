43980ab6c19c0c379836bf0c6afe487ef1fe22a0
alee <alee@aleeui-MacBookPro.local>
2022-12-12 08:03:30 +0900

    feat: recipe title, recipe menu

---
+++ flutter_recipe/lib/components/recipe_list_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem({
+    Key? key,
+    required this.imageName,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

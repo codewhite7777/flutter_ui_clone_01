78a214582617e0031263c30e105b4e3a7a35acc9
alee <alee@aleeui-MacBookPro.local>
2022-12-13 03:09:24 +0900

    feat: custom text form field - components

---
+++ flutter_login/lib/components/custom_form.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/components/custom_text_form_field.dart';
+import 'package:flutter_login/size.dart';
+
+class CustomForm extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  CustomForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField(text: 'Email'),
+          SizedBox(height: midium_gap),
+          CustomTextFormField(text: 'Password'),
+          SizedBox(height: large_gap),
+        ],
+      ),
+    );
+  }
+}

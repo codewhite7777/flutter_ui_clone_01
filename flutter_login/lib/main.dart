68ce578f4a1bb0b88dd60e4f8ae1f418ebe1960e
alee <alee@aleeui-MacBookPro.local>
2022-12-13 01:28:27 +0900

    feat: logo components

--- flutter_login/lib/main.dart
+++ flutter_login/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/home_page.dart';
+import 'package:flutter_login/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
 }

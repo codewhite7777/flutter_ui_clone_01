7303b6a02055c391c6cbd69948d0af40de05233d
alee <alee@aleeui-MacBookPro.local>
2022-12-14 05:55:43 +0900

    feat: home header - shape

---
+++ flutter_airbnb/lib/components/home/home_header_appbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/size.dart';
+
+class HomeHeaderAppBar extends StatelessWidget {
+  const HomeHeaderAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(gap_m),
+      child: Row(
+        children: [
+          _buildAppBarLogo(),
+          Spacer(),
+          _buildAppBarMenu(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppBarLogo() {
+    return SizedBox();
+  }
+
+  Widget _buildAppBarMenu() {
+    return SizedBox();
+  }
+}

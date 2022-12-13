0b65453992022d1cf2ceb4e693dce8aa89d55d78
alee <alee@aleeui-MacBookPro.local>
2022-12-13 10:11:01 +0900

    feat: shpping cart detail

--- flutter_shoppingcart/lib/components/shoppingcart_detail.dart
+++ flutter_shoppingcart/lib/components/shoppingcart_detail.dart
@@ -1,10 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/constants.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
   const ShoppingCartDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingAndReviewCount(),
+            _buildDetailColorOption(),
+            _buildDetailButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailNameAndPrice() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Urban Soft AL 10.0',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '\$699',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailRatingAndReviewCount() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: Row(
+        children: [
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          Spacer(),
+          Text('Review'),
+          Text(
+            '(26)',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailColorOption() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Color Options'),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              _buildDetailIcon(Colors.black),
+              _buildDetailIcon(Colors.green),
+              _buildDetailIcon(Colors.orange),
+              _buildDetailIcon(Colors.grey),
+              _buildDetailIcon(Colors.white),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailIcon(Color mColor) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Stack(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(),
+              shape: BoxShape.circle,
+            ),
+          ),
+          Positioned(
+            left: 5,
+            top: 5,
+            child: ClipOval(
+              child: Container(
+                width: 40,
+                height: 40,
+                color: mColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          minimumSize: Size(400, 50),
+          backgroundColor: kAccentColor,
+        ),
+        onPressed: () {},
+        child: Text(
+          'Add to Cart',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }

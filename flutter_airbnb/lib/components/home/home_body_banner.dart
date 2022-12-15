5bc519f4d61bcd3147fd996832408e9b1203b204
alee <alee@aleeui-MacBookPro.local>
2022-12-16 02:49:34 +0900

    feat: home body - banner

--- flutter_airbnb/lib/components/home/home_body_banner.dart
+++ flutter_airbnb/lib/components/home/home_body_banner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/style.dart';
 
 class HomeBodyBanner extends StatelessWidget {
   const HomeBodyBanner({Key? key}) : super(key: key);
@@ -18,10 +19,55 @@ class HomeBodyBanner extends StatelessWidget {
   }
 
   Widget _buildBannerImage() {
-    return SizedBox();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.asset('asset/images/banner.jpg',
+          fit: BoxFit.cover, width: double.infinity, height: 320),
+    );
   }
 
   Widget _buildBannerCaption() {
-    return SizedBox();
+    return Positioned(
+      top: 40,
+      left: 40,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              '이제, 여행은 가까운 곳에서',
+              style: h4(mColor: Colors.white),
+            ),
+          ),
+          SizedBox(height: gap_m),
+          Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              '새로운 공간에 머물러 보세요. 살아보기, 출장, 여행 등 다양한 목적에 맞는 숙소를 찾아보세요.',
+              style: subtitle1(mColor: Colors.white),
+            ),
+          ),
+          SizedBox(height: gap_m),
+          SizedBox(
+            height: 35,
+            width: 170,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                '가까운 여행지 둘러보기',
+                style: subtitle2(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

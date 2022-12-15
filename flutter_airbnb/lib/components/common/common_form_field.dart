8d451ec0d2cffd822acb52697fc9a5cb416369a1
alee <alee@aleeui-MacBookPro.local>
2022-12-16 00:43:15 +0900

    feat: common form field

--- flutter_airbnb/lib/components/common/common_form_field.dart
+++ flutter_airbnb/lib/components/common/common_form_field.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/style.dart';
 
 class CommonField extends StatelessWidget {
-  const CommonField({Key? key}) : super(key: key);
+  final prefixText;
+  final hintText;
+
+  const CommonField({
+    Key? key,
+    this.prefixText,
+    this.hintText,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        TextFormField(
+          textAlignVertical: TextAlignVertical.bottom,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(top: 30, left: 20, bottom: 10),
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 20,
+          child: Text(
+            prefixText,
+            style: overline(),
+          ),
+        )
+      ],
+    );
   }
 }

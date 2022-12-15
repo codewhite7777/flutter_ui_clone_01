df29fd6271449986f6643359f3671839ccbb6ff9
alee <alee@aleeui-MacBookPro.local>
2022-12-16 08:45:22 +0900

    feat: class User

--- flutter_kakao/lib/screens/main_screen.dart
+++ flutter_kakao/lib/screens/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/chat_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/more_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
@@ -17,15 +20,9 @@ class _MainScreenState extends State<MainScreen> {
       body: IndexedStack(
         index: _selectedIndex,
         children: [
-          Center(
-            child: Text('IndexedStack 1'),
-          ),
-          Center(
-            child: Text('IndexedStack 2'),
-          ),
-          Center(
-            child: Text('IndexedStack 3'),
-          ),
+          FriendScreen(),
+          ChatScreen(),
+          MoreScreen(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(

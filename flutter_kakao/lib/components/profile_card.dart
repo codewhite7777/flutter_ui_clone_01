d2d4a2c1fde30073f6ba23fb95aac4803ed88c18
alee <alee@aleeui-MacBookPro.local>
2022-12-16 11:53:36 +0900

    feat: friend screen

--- flutter_kakao/lib/components/profile_card.dart
+++ flutter_kakao/lib/components/profile_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kakao/model/user.dart';
+import 'package:flutter_kakao/screens/profile_screen.dart';
 
 class ProfileCard extends StatelessWidget {
   final User user;
@@ -11,22 +12,31 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        print('${user.name} clicked');
-      },
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(user.backgoundImage),
-          radius: 20,
-        ),
-        title: Text(
-          user.name,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-        subtitle: Text(
-          user.intro,
-          style: TextStyle(fontSize: 12),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext) {
+                return ProfileScreen(user: user);
+              },
+            ),
+          );
+        },
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(user.backgoundImage),
+            radius: 20,
+          ),
+          title: Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          subtitle: Text(
+            user.intro,
+            style: TextStyle(fontSize: 12),
+          ),
         ),
       ),
     );
---
+++ flutter_kakao/lib/screens/profile_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/bottom_icon_button.dart';
+import 'package:flutter_kakao/components/round_icon_button.dart';
+import 'package:flutter_kakao/model/user.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final User user;
+
+  const ProfileScreen({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(user.backgoundImage),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              FontAwesomeIcons.xmark,
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+          actions: [
+            RoundIconButton(icon: FontAwesomeIcons.gift),
+            SizedBox(width: 15),
+            RoundIconButton(icon: FontAwesomeIcons.gear),
+            SizedBox(width: 20),
+          ],
+        ),
+        body: Column(
+          children: [
+            Spacer(),
+            Container(
+              width: 110,
+              height: 110,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(user.backgoundImage),
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              user.name,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 5),
+            Text(
+              user.intro,
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+            SizedBox(height: 20),
+            Divider(color: Colors.white),
+            user.name == me.name ? _buildMyIcons() : _buildFriendIcons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMyIcons() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BottomIconButton(icon: FontAwesomeIcons.comment, text: '나와의 채팅'),
+          SizedBox(width: 50),
+          BottomIconButton(icon: FontAwesomeIcons.pen, text: '프로필 편집'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFriendIcons() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BottomIconButton(icon: FontAwesomeIcons.comment, text: '1:1 채팅'),
+          SizedBox(width: 50),
+          BottomIconButton(icon: FontAwesomeIcons.phone, text: '통화하기'),
+        ],
+      ),
+    );
+  }
+}
